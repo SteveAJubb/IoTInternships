304d3a77d30dfedc4bfe33f8730015d2ad63a8e9
harrison.fret@gmail.com <harrison.fret@gmail.com>
2020-07-29 14:33:03 +0100

    Merging old changes

--- AMRC-Monitoring/api/app.js
+++ AMRC-Monitoring/api/app.js
@@ -45,17 +45,12 @@ app.use((req, res, next) => {
   return next()
 })
 
-<<<<<<< HEAD
-
 app.post('/ttn',(req,res) => {
   console.log(req.body)
 })
 
-=======
-//*All below routes are largely for testing
 let oldTemp = 0
 let temperature = Math.random()*100;
->>>>>>> c5f073d0a6ac8c2687496a96278f7152f24298f3
 //TEMP: Dummy test input of temperature data
 app.get('/test-input', (req,res)=> {
     let timeout = setInterval(() => {
